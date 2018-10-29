4a28387483a1fd606e3f46a374059562a74fdd7b
p-morris <pete@iosfaststart.com>
2018-10-29 16:45:27 +0800

    Added network settings object

--- iOS Video Interstitial Advert Mediator/VideoAdNetwork.swift
+++ iOS Video Interstitial Advert Mediator/VideoAdNetwork.swift
@@ -8,9 +8,9 @@
 
 import Foundation
 
-@objc protocol AdNetwork {
-    /// The object that acts as the delegate of the `AdNetwork`.
-    var delegate: AdNetworkDelegate? { get set }
+@objc protocol VideoAdNetwork {
+    /// The object that acts as the delegate of the `VideoAdNetwork`.
+    var delegate: VideoAdNetworkDelegate? { get set }
     /**
      Sends a request to the ad network for a advert.
      */
@@ -18,21 +18,21 @@ import Foundation
 }
 
 /// Provides callbacks for AdNetwork request events.
-@objc protocol AdNetworkDelegate {
+@objc protocol VideoAdNetworkDelegate {
     /**
      Executed when the ad network request is fulfilled.
      
      - Parameters:
-     - adNetwork: The `AdNetwork` responsible for the callback.
+     - adNetwork: The `VideoAdNetwork` responsible for the callback.
      - ad: A `VideoAd` object ready for display.
      */
-    func adNetwork(_ adNetwork: AdNetwork, didLoad ad: VideoAd)
+    func adNetwork(_ adNetwork: VideoAdNetwork, didLoad ad: VideoAd)
     /**
      Executed when the ad network request either fails, or is unfulfilled.
      
      - Parameters:
-     - adNetwork: The `AdNetwork` responsible for the callback.
+     - adNetwork: The `VideoAdNetwork` responsible for the callback.
      - error: An `Error` representing the problem that occured.
      */
-    func adNetwork(_ adNetwork: AdNetwork, didFailToLoad error: Error)
+    func adNetwork(_ adNetwork: VideoAdNetwork, didFailToLoad error: Error)
 }
