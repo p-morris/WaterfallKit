d3e1d6e35156b81a1abf92eb50485b5f8512c722
p-morris <pete@iosfaststart.com>
2018-11-02 16:43:26 +0800

    added docs for timeout timer

--- iOS Video Interstitial Advert Mediator/TimeOutTimer.swift
+++ iOS Video Interstitial Advert Mediator/TimeOutTimer.swift
@@ -8,18 +8,36 @@
 
 import Foundation
 
+/// Encapsulates an instance on Timer
 class TimeOutTimer {
-    static let timeOutError = "TimeOutErrorDomain"
+    /// The timer to use for the timeout
     private var timer: Timer?
+    /// The `TimeInterval` to wait before timing out.
     let timeOutIn: TimeInterval
+    /**
+     Initializes a new `TimeOutTimer` object.
+     
+     - Parameters:
+     - timeOutIn: The `TimeInterval` to wait before timing out.
+     - Returns: An initialized `TimeOutTimer` object.
+     */
     init(timeOutIn: TimeInterval) {
         self.timeOutIn = timeOutIn
     }
+    /**
+     Begins the timeout.
+     
+     - Parameters:
+     - timeoutable: The `TimeOutableVideoAdNetworkAdapter` to notify on timeout.
+     */
     func startTimeOut(notify timeoutable: TimeOutableVideoAdNetworkAdapter) {
         timer = Timer.scheduledTimer(withTimeInterval: timeOutIn, repeats: false, block: { _ in
             timeoutable.timeOut()
         })
     }
+    /**
+     Cancels the timeout.
+     */
     func cancelTimeOut() {
         timer?.invalidate()
     }
